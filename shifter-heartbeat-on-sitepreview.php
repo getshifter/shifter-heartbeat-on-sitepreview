fb2f96d25e1dd302a30f667161e0724a75e58586
Yukihiko SAWANOBORI <sawanoboriyu@gmail.com>
2017-11-09 18:13:15 +0900

    no echo

--- shifter-heartbeat-on-sitepreview.php
+++ shifter-heartbeat-on-sitepreview.php
@@ -5,18 +5,17 @@ Plugin Name: Shifter HeartBeat on SitePreview
 
 function shifter_heartbert_on_sitepreview_writeScript() {
   if (is_user_logged_in()) {
-  echo '
-  <script>
-  function shifter_heartbert_getajax() {
-    var xhr= new XMLHttpRequest();
-    xhr.open("GET","/wp-admin/admin-ajax.php");
-    xhr.send();
-    }
-  setInterval("shifter_heartbert_getajax()",60000);
-  </script>
-  ';
+?>
+<script>
+function shifter_heartbert_getajax() {
+  var xhr= new XMLHttpRequest();
+  xhr.open("GET","/wp-admin/admin-ajax.php");
+  xhr.send();
+  }
+setInterval("shifter_heartbert_getajax()",60000);
+</script>
+<?php
   }
 }
 add_action( 'wp_footer', 'shifter_heartbert_on_sitepreview_writeScript', 999 );
-
 ?>
